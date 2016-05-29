30d01ead40df98b0b23693c30b46a6f9026192ce
蒋国宝 <loveaborn@foxmail.com>
2016-05-29 10:34:17 +0800

    update

--- scripts/sync-melpa.sh
+++ scripts/sync-melpa.sh
@@ -5,8 +5,8 @@ git pull
 echo `date +%Y-%m-%d-%H:%M.%S`
 cd /home/popkit/melpa
 git pull
-cp recipes/* /home/popkit/popkit-elpa/recipes
-cd /home/popkit/popkit-elpa
+cp recipes/* /home/popkit/pelpa/recipes
+cd /home/popkit/pelpa
 git add -A .
-git commit -am "sync melpa.org recipes `date +%Y-%m-%d-%H:%M.%S`"
+git commit -am "sync melpa.org recipes @ `date +%Y-%m-%d-%H:%M.%S`"
 git push
