da3e8900f3fda40ec420f5e3ce72eb0b3e58835c
蒋国宝 <loveaborn@foxmail.com>
2016-05-30 09:09:02 +0800

    support MercurialFetchHandler

--- pelpa-web/src/main/java/org/popkit/leap/elpa/services/handler/MercurialFetchHandler.java
+++ pelpa-web/src/main/java/org/popkit/leap/elpa/services/handler/MercurialFetchHandler.java
@@ -1,8 +1,13 @@
 package org.popkit.leap.elpa.services.handler;
 
+import org.popkit.core.entity.SimpleResult;
+import org.popkit.core.logger.LeapLogger;
 import org.popkit.leap.elpa.entity.FetcherEnum;
 import org.popkit.leap.elpa.entity.RecipeDo;
 import org.popkit.leap.elpa.services.FetchHandler;
+import org.popkit.leap.elpa.services.RecipesService;
+import org.popkit.leap.elpa.utils.HGCommandUtils;
+import org.popkit.leap.elpa.utils.PelpaUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -25,6 +30,21 @@ public class MercurialFetchHandler implements FetchHandler {
     }
 
     public void execute(RecipeDo recipeDo, Map<String, Object> extra) {
+        try {
+            SimpleResult simpleResult = HGCommandUtils.hgFetchRemote2Local(recipeDo);
+            if (simpleResult.isSuccess()) {
+                long lastModified = HGCommandUtils.getHGLastModified(PelpaUtils.getWorkingPath(recipeDo.getPkgName()));
+                if (lastModified > 0) {
+                    boolean status = RecipesService.updateLastCommit(recipeDo.getPkgName(), lastModified);
+                    LeapLogger.info("update " + recipeDo.getPkgName() + " lastcommit, status=" + status);
+                } else {
+                    LeapLogger.warn("mercurialFetchHandler pkg(" + recipeDo.getPkgName() + ") failed!");
+                }
+            }
+        } catch (Exception e) {
+            LeapLogger.warn("mercurialFetchHandler pkg(" + recipeDo.getPkgName() + ") exception!");
+        }
 
+        LeapLogger.info("MercurialFetchHandler finished:" + recipeDo.getPkgName());
     }
 }
--- pelpa-web/src/main/java/org/popkit/leap/elpa/utils/HGCommandUtils.java
+++ pelpa-web/src/main/java/org/popkit/leap/elpa/utils/HGCommandUtils.java
@@ -1,9 +1,11 @@
 package org.popkit.leap.elpa.utils;
 
+import org.apache.commons.io.FileUtils;
 import org.popkit.core.entity.SimpleResult;
 import org.popkit.leap.elpa.entity.RecipeDo;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.ParseException;
@@ -19,24 +21,67 @@ public class HGCommandUtils {
     public static final String BITBUCKET_HTTPS_ROOT = "https://bitbucket.org/";
 
     public static void main(String[] args) {
-        long time = getLastModified("/Users/aborn/tmp/evil");
+        String pkgName = "evil";
+        RecipeDo re = new RecipeDo();
+        re.setPkgName(pkgName);
+        re.setRepo("lyro/evil");
+        SimpleResult simpleResult = hgFetchRemote2Local(re);
+        long time = getHGLastModified(PelpaUtils.getWorkingPath(re.getPkgName()));
+        System.out.println("simpleResult:" + simpleResult);
         System.out.println("date=" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(time));
     }
 
-    public static SimpleResult hgClone(RecipeDo recipeDo) {
+
+    public static SimpleResult hgFetchRemote2Local(RecipeDo recipeDo) {
         String url = BITBUCKET_HTTPS_ROOT + recipeDo.getRepo();
-        return SimpleResult.fail("go");
+        String wokingPath = PelpaUtils.getWorkingPath(recipeDo.getPkgName());
+        // hg clone https://bitbucket.org/lyro/evil /Users/aborn/tmp/evil
+        // hg pull --cwd /Users/aborn/tmp/evil
+
+        String hgCloneCmd = "hg clone " + url + " " + wokingPath;
+        String hgPullCmd = "hg pull --cwd " + wokingPath;
+        String command;
+        if (existsHgRepo(wokingPath)) {
+            command = hgPullCmd;
+            if (new File(wokingPath).exists()) {
+                try {
+                    FileUtils.deleteDirectory(new File(wokingPath));
+                } catch (IOException e) {
+                    //
+                }
+            }
+        } else {
+            command = hgCloneCmd;
+        }
+
+        try {
+            Process p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            if (existsHgRepo(wokingPath)) {
+                return SimpleResult.success("hg clone success");
+            }
+        } catch (InterruptedException e) {
+        } catch (IOException ioe) {
+        }
+        return SimpleResult.fail("error in hgclone for pkgName=" + recipeDo.getPkgName());
     }
 
-    public static long getLastModified(String localPath) {
-        String comm = "hg log -l 1 -b .";
-        //String comm = "pwd ";
-        // Wed May 25 14:48:02 2016 -0400
-        String originWoringPath = System.getProperty("user.dir");
+    public static boolean existsHgRepo(String wokingPath) {
+        File _hg = new File(wokingPath + "/.hg");
+        File _hg_woking = new File(wokingPath);
+        return _hg_woking.exists() && _hg.exists()
+                && _hg.isDirectory() && _hg_woking.isDirectory();
+    }
+
+    public static long getHGLastModified(String localPath) {
+        String comm = "hg log -l 1 " + localPath;
         try {
-            System.out.println("originWoringPath=" + originWoringPath);
-            System.setProperty("user.dir", localPath);
-            System.out.println("current=" + System.getProperty("user.dir"));
             Process p = Runtime.getRuntime().exec(comm);
             p.waitFor();
             BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
@@ -49,7 +94,7 @@ public class HGCommandUtils {
                 System.out.println(line);
             }
             if (null != lastModifiedString) {
-                SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss 2016 Z");
+                SimpleDateFormat format = new SimpleDateFormat("EEE MMM dd HH:mm:ss yyyy Z");
                 Date d = format.parse(lastModifiedString);
                 System.out.println(d);
                 return d.getTime();
@@ -61,7 +106,7 @@ public class HGCommandUtils {
         } catch (IOException ioe) {
             return 0;
         } finally {
-            System.setProperty("user.dir", originWoringPath);
+            //
         }
         return 0;
     }
