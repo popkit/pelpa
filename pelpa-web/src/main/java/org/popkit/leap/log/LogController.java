baaf0ba70c209efc8a88df454439c36f9f7a8aac
蒋国宝 <loveaborn@foxmail.com>
2016-06-04 21:32:10 +0800

    monthly statistics

--- pelpa-web/src/main/java/org/popkit/leap/log/LogController.java
+++ pelpa-web/src/main/java/org/popkit/leap/log/LogController.java
@@ -4,6 +4,8 @@ import com.alibaba.fastjson.JSON;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.io.FileUtils;
 import org.joda.time.DateTime;
+import org.joda.time.Minutes;
+import org.popkit.core.logger.LeapLogger;
 import org.popkit.core.utils.ResponseUtils;
 import org.popkit.leap.common.BaseController;
 import org.popkit.leap.elpa.utils.PelpaUtils;
@@ -12,10 +14,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Aborn Jiang
@@ -25,6 +31,7 @@ import java.util.*;
 @Controller
 @RequestMapping("elpa/log")
 public class LogController extends BaseController {
+    public static final ScheduledExecutorService scheduledExecutor = Executors.newScheduledThreadPool(1);
 
     @Autowired
     private LogScanner logScanner;
@@ -33,7 +40,7 @@ public class LogController extends BaseController {
     public void ajaxmonthss(HttpServletResponse response) {
         DateTime now = new DateTime();
         DateTime startTime = now.minusDays(30).withTimeAtStartOfDay();
-        DateTime endTime = now.plusDays(1).withTimeAtStartOfDay();
+        DateTime endTime = now.withTimeAtStartOfDay();
 
         List<String> labels = new ArrayList<String>();
         List<Integer> data = new ArrayList<Integer>();
@@ -58,14 +65,36 @@ public class LogController extends BaseController {
         }
 
         EachLine eachLine = new EachLine(labels, data);
-        eachLine.setLabel(startTime.toString(LogScanner.DAY_FORMAT) + "~" + endTime.toString(LogScanner.DAY_FORMAT));
+        eachLine.setLabel(startTime.toString(LogScanner.DAY_FORMAT) + "~" + endTime.minusDays(1).toString(LogScanner.DAY_FORMAT));
         ResponseUtils.renderJson(response, eachLine.toString());
     }
 
+    @PostConstruct
+    public void init() {
+        DateTime now = new DateTime();
+        DateTime tomorrowFirstTime = now.plusDays(1).withTimeAtStartOfDay().plusMinutes(3);
+        Minutes delayMinutes = Minutes.minutesBetween(now, tomorrowFirstTime);
+
+        scheduledExecutor.scheduleWithFixedDelay(new Runnable() {
+            public void run() {
+                //LeapLogger.info("exectud!!" + new DateTime().toString(DateTimeFormat.forPattern("yyyy-MM-dd hh:mm:ss")));
+            }
+        }, 0, 5, TimeUnit.MINUTES);   // execute each 5 minutes.
+
+        // init executed it and periodic each day executed
+        generateLatestStaticsJSON();
+        scheduledExecutor.scheduleWithFixedDelay(new Runnable() {
+            public void run() {
+                generateLatestStaticsJSON();
+            }
+        }, delayMinutes.getMinutes(), 24*60, TimeUnit.MINUTES);   // execute each day
+    }
+
     public void generateLatestStaticsJSON() {
+        LeapLogger.info("#generateLatestStaticsJSON# executed!");
         DateTime now = new DateTime();
         DateTime startTime = now.minusDays(30).withTimeAtStartOfDay();
-        DateTime endTime = now.plusDays(1).withTimeAtStartOfDay();
+        DateTime endTime = now.withTimeAtStartOfDay();
         DateTime tmp = startTime;
         List<DateTime> timeList = new ArrayList<DateTime>();
         while (tmp.isBefore(endTime)) {
