5e24042055877ec6a0f6eb5db0537d778717b2da
guobao.jiang <loveaborn@foxmail.com>
2016-06-17 09:43:16 +0800

    update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -27,12 +27,6 @@ mvn compile
 # package war
 mvn package -Dmaven.test.skip=ture
 
-# remove old file
-rm -rf ${CATALINA_HOME}/webapps/pelpa
-
-# use new war files
-cp -r pelpa-web/target/pelpa ${CATALINA_HOME}/webapps/
-
 # stop tomcat and start it
 ${CATALINA_HOME}/bin/catalina.sh stop
 kill -9 `ps -ef |grep tomcat|grep -v grep|awk '{print $2}'`
