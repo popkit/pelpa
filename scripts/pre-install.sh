f52964a2a623dd241c655255939f3178b491371a
Aborn Jiang <loveaborn@foxmail.com>
2016-06-30 07:47:14 +0800

    example

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -3,3 +3,4 @@
 sudo apt-get install subversion
 
 # svn checkout http://svn.osdn.jp/svnroot/macemacsjp/applescript-mode/trunk
+# svn checkout http://svn.ruby-lang.org/repos/ruby/trunk/misc/ ruby-electric
