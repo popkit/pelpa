d805098bcf15f5f3ac18598144eed4f091e76ab3
Aborn Jiang <loveaborn@foxmail.com>
2016-07-03 12:19:01 +0800

    bzr && cvs

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -4,3 +4,13 @@ sudo apt-get install subversion
 
 # svn checkout http://svn.osdn.jp/svnroot/macemacsjp/applescript-mode/trunk
 # svn checkout http://svn.ruby-lang.org/repos/ruby/trunk/misc/ ruby-electric
+
+# install bzr
+sudo apt-get install bzr
+
+# bzr branch  lp:weblogger-el weblogger
+# bzr branch lp:~nxhtml/nxhtml/main sml-modeline
+# bzr branch http://bzr.savannah.gnu.org/r/color-theme/trunk color-theme
+
+# install cvs
+sudo apt-get install cvs
