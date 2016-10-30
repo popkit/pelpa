cc0a6aa4bdfc1a751aee7cf82725c46aeefa7e79
Aborn Jiang <loveaborn@foxmail.com>
2016-10-30 14:19:31 +0800

    删除无用的

--- pelpa-web/src/main/java/org/popkit/leap/monitor/FetcherExcutorPool.java
+++ pelpa-web/src/main/java/org/popkit/leap/monitor/FetcherExcutorPool.java
@@ -7,8 +7,6 @@ import org.popkit.leap.elpa.services.PkgFetchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -20,36 +18,10 @@ import java.util.concurrent.TimeUnit;
  */
 @Service
 public class FetcherExcutorPool {
-    private static final int GROUP_SIZE = 20;
-
     @Autowired
     private PkgFetchService pkgFetchService;
 
     private final ExecutorService EXECUTOR_POOL = Executors.newFixedThreadPool(4);
-
-    public ExecutorService getExector() {
-        return EXECUTOR_POOL;
-    }
-
-    private List<FetcherTask> getFetcherGroupTask() {
-        int i = GROUP_SIZE;
-        List<FetcherTask> result = new ArrayList<FetcherTask>();
-        while (i > 0) {
-            String pkgName = RoundStatusMonitor.nexFetcherPkg();
-            if (StringUtils.isBlank(pkgName)) {
-                return result;
-            }
-            ActorStatus actorStatus = RoundStatusMonitor.getFetcherStatus(pkgName);
-            if (actorStatus == ActorStatus.READY) {
-                LeapLogger.info("pkgName:" + pkgName + " added to fetch working queue!");
-                RoundStatusMonitor.updateFetcherStatus(pkgName, ActorStatus.WORKING);
-                FetcherTask fetcherTask = new FetcherTask(pkgName, pkgFetchService);
-                result.add(fetcherTask);
-            }
-            i--;
-        }
-        return result;
-    }
 
     public void excute() {
         // // FIXME: 6/9/16 这里有个bug,一个pkg可能会循环加入fetch任务队列
@@ -70,37 +42,6 @@ public class FetcherExcutorPool {
                     if (StringUtils.isBlank(pkgName)) {
                         continue;
                     }
-
-                    /*
-                    List<FetcherTask> fetcherTasks = getFetcherGroupTask();
-                    if (CollectionUtils.isEmpty(fetcherTasks)) {
-                        continue;
-                    }
-
-                    try {
-                        Iterator<FetcherTask> iterator = fetcherTasks.iterator();
-                        List<Future<FetcherStatus>> futures = EXECUTOR_POOL.invokeAll(fetcherTasks, 10, TimeUnit.MINUTES);
-                        for (Future<FetcherStatus> itemFuture : futures) {
-                            FetcherTask task = iterator.next();
-                            try {
-                                FetcherStatus status = itemFuture.get();
-                                if (status == null) {
-                                    LeapLogger.warn("fetcher [" + task.getPkgName() + "] timeout!!");
-                                    // 这里也更新状态为成功
-                                } else {
-                                    LeapLogger.warn("fetcher [" + task.getPkgName() + "] success!!" + status.getInfo());
-                                }
-                            } catch (ExecutionException e) {
-                                LeapLogger.warn("fetcher [" + task.getPkgName() + "] timeout!! exception..");
-                                e.printStackTrace();
-                            } finally {
-                                RoundStatusMonitor.updateFetcherStatus(task.getPkgName(), ActorStatus.FINISHED);
-                            }
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    */
 
                     ActorStatus actorStatus = RoundStatusMonitor.getFetcherStatus(pkgName);
                     if (actorStatus == ActorStatus.READY) {
