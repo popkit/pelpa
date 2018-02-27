35310798614573e946de1571085e6b304033bf20
guobao.jiang <guobao.jiang@dianping.com>
2018-02-27 13:04:48 +0800

    配置与代码分离

--- pelpa-web/src/main/java/org/popkit/leap/wechart/db/MyDataSource.java
+++ pelpa-web/src/main/java/org/popkit/leap/wechart/db/MyDataSource.java
@@ -2,6 +2,7 @@ package org.popkit.leap.wechart.db;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.lang3.StringUtils;
+import org.popkit.core.config.LeapConfigLoader;
 import org.springframework.beans.factory.InitializingBean;
 
 import java.sql.SQLFeatureNotSupportedException;
@@ -22,17 +23,22 @@ public class MyDataSource extends BasicDataSource implements InitializingBean {
     @Override
     public void afterPropertiesSet() throws Exception {
         String url = this.getUrl();
+        String env = LeapConfigLoader.get("env", true);
+        if (StringUtils.isBlank(env)) {
+            return;
+        }
+
         // 下面设置具体的值
         if (StringUtils.isBlank(url)) {
-            this.setUrl("");
+            this.setUrl(LeapConfigLoader.get("jdbc.url." + env));
         }
         String username = this.getUsername();
         if (StringUtils.isBlank(username)) {
-            this.setUsername("");
+            this.setUsername(LeapConfigLoader.get("jdbc.name." + env));
         }
         String password = this.getPassword();
         if (StringUtils.isBlank(password)) {
-            this.setPassword("");
+            this.setPassword(LeapConfigLoader.get("jdbc.pwd." + env));
         }
     }
 }
